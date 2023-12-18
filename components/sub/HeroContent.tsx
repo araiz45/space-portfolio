afeac1268ce3942103a74694b3636d9becca45a4
araiz45 <araizzahid45@gmail.com>
2023-12-18 20:06:05 +0500

    Website Completed 2

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -43,8 +43,8 @@ export default function HeroContent() {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I'm a Full Stack Software Engineer with experience in Website, Mobile
-          and Software development. Check out my project and skills
+          I&apos;m a Full Stack Software Engineer with experience in Website,
+          Mobile and Software development. Check out my project and skills
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
